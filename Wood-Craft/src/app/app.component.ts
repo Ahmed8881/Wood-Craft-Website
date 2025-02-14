2ecf7ed5725b3c203a4f93db70b9e4f077d5cc6b
Ahmed8881 <buttahmed2004@gmail.com>
2025-02-14 18:54:18 +0500

    add products component; define Product interface and initialize product list

--- Wood-Craft/src/app/app.component.ts
+++ Wood-Craft/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { HeroComponent } from './hero/hero.component';
+import { ProductsComponent } from "./products/products.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,HeroComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Wood-Craft/src/app/products/products.component.ts
+++ Wood-Craft/src/app/products/products.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+
+interface Product {
+  id: number
+  name: string
+  price: string
+  description: string
+  image: string
+}
 
 @Component({
-  selector: 'app-products',
+  selector: "app-products",
   standalone: true,
-  imports: [],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.scss"],
 })
 export class ProductsComponent {
-
+  products: Product[] = [
+    {
+      id: 1,
+      name: "Classic Wooden Keychain",
+      price: "$19.99",
+      description: "Premium bamboo with custom laser engraving",
+      image: "/assets/images/classic-keychain.jpg",
+    },
+    {
+      id: 2,
+      name: "Metallic Edge Keychain",
+      price: "$24.99",
+      description: "Wood-metal fusion with personalized text",
+      image: "/assets/images/metallic-keychain.jpg",
+    },
+    {
+      id: 3,
+      name: "Photo Engraved Keychain",
+      price: "$29.99",
+      description: "Your favorite photo laser-etched on wood",
+      image: "/assets/images/photo-keychain.jpg",
+    },
+    {
+      id: 4,
+      name: "QR Code Keychain",
+      price: "$22.99",
+      description: "Custom QR code with wooden finish",
+      image: "/assets/images/qr-keychain.jpg",
+    },
+  ]
 }
+
